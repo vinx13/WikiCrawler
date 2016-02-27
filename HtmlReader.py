e6ea374b05aaa27287e9efb2835d549bf57d12f7
Wuwei Lin <vincentl13x@gmail.com>
2016-02-27 14:04:54 +0800

    Add html reader

---
+++ HtmlReader.py
@@ -0,0 +1,12 @@
+import urllib2
+
+
+class HtmlReader(object):
+    URL_BASE = "https://en.wikipedia.org/"
+
+    def __init__(self, url):
+        self.url = self.URL_BASE + url
+
+    def read(self):
+        request = urllib2.Request(self.url)
+        return urllib2.urlopen(request).read()
